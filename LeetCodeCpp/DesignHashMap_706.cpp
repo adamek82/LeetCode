d05c451548e91dcae51e873df66579fac539580a
adamek82 <amichalski@gmail.com>
2025-12-29 19:06:59 +0100

    Design Hash Map (#706): document constructor table sizing and sentinel init
    Explain why the direct-address vector is sized kMaxKey + 1 (keys 0..kMaxKey inclusive)
    and that the vector (size, value) ctor pre-fills all slots with the kMissing sentinel (-1),
    which is safe given the value constraints.

--- LeetCodeCpp/DesignHashMap_706.cpp
+++ LeetCodeCpp/DesignHashMap_706.cpp
@@ -1,6 +1,11 @@
 #include "DesignHashMap_706.h"
 
 MyHashMap::MyHashMap()
+    /*
+     * We need indices 0..kMaxKey inclusive, so the table size is kMaxKey + 1.
+     * The vector constructor (size, value) fills all entries with kMissing (-1),
+     * which is safe because valid values are in [0..1_000_000].
+     */
     : values_(kMaxKey + 1, kMissing) {
 }
 
