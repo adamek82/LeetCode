6b649c866099c603369feb15a01fc1022286082c
adamek82 <amichalski@gmail.com>
2026-07-23 11:14:36 +0200

    Simplify FizzBuzz conditions

--- LeetCodeCpp/FizzBuzz_412.cpp
+++ LeetCodeCpp/FizzBuzz_412.cpp
@@ -5,14 +5,15 @@ vector<string> FizzBuzz_412::fizzBuzz(int n)
     vector<string> ans;
     ans.reserve(n);
 
-    for (int i=1; i<=n; ++i) {
-        bool fizz = (i % 3 == 0);
-        bool buzz = (i % 5 == 0);
+    for (int i = 1; i <= n; ++i) {
+        const bool fizz = (i % 3 == 0);
+        const bool buzz = (i % 5 == 0);
+
         if (fizz && buzz)
             ans.push_back("FizzBuzz");
-        else if (fizz && !buzz)
+        else if (fizz)
             ans.push_back("Fizz");
-        else if (!fizz && buzz)
+        else if (buzz)
             ans.push_back("Buzz");
         else
             ans.push_back(to_string(i));
