adab1767c02ae41c68d10397436e7572df2e120e
adamek82 <amichalski@gmail.com>
2026-07-27 14:10:13 +0200

    Refactor sort colors implementations

--- LeetCodeCpp/SortColors_75.h
+++ LeetCodeCpp/SortColors_75.h
@@ -5,9 +5,6 @@ using namespace std;
 
 class SortColors_75 {
 public:
-    // 1-pass overwrite with three tails
     void sortColors_threeTails(vector<int>& nums);
-
-    // 2-pass counting (count then fill)
     void sortColors_counting(vector<int>& nums);
 };
