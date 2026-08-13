7308ba935526d208eb272eb5e21b8ef9286f8a6b
adamek82 <amichalski@gmail.com>
2026-08-13 20:11:58 +0200

    Refactor LeetCode 235 lowest common ancestor solution

--- LeetCodeCpp/problems/trees/LowestCommonAncestorOfBST_235.h
+++ LeetCodeCpp/problems/trees/LowestCommonAncestorOfBST_235.h
@@ -1,7 +1,11 @@
 #pragma once
+
 #include "common/TreeNode.h"
 
 class LowestCommonAncestorOfBST_235 {
 public:
-    TreeNode<int>* lowestCommonAncestor(TreeNode<int>* root, TreeNode<int>* p, TreeNode<int>* q);
+    TreeNode<int>* lowestCommonAncestor(
+        TreeNode<int>* root,
+        TreeNode<int>* p,
+        TreeNode<int>* q);
 };
