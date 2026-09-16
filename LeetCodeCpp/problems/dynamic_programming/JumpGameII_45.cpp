d1f78b1b17fabff70fd1ae1d626b9a8cee49fd86
adamek82 <amichalski@gmail.com>
2026-09-16 13:34:09 +0200

    Simplify Jump Game II BFS layer naming

--- LeetCodeCpp/problems/dynamic_programming/JumpGameII_45.cpp
+++ LeetCodeCpp/problems/dynamic_programming/JumpGameII_45.cpp
@@ -6,14 +6,14 @@ int JumpGameII_45::jump(vector<int>& nums) {
     if (n <= 1) return 0;
 
     int jumps = 0;
-    int curEnd = 0;    // farthest index reachable with exactly `jumps` jumps
-    int farthest = 0;  // farthest index reachable with `jumps + 1` jumps
+    int layerEnd = 0;
+    int nextLayerEnd = 0;
 
     for (int i = 0; i < n - 1; ++i) {
-        farthest = max(farthest, i + nums[i]);
-        if (i == curEnd) {      // end of current layer -> must "take" a jump
+        nextLayerEnd = max(nextLayerEnd, i + nums[i]);
+        if (i == layerEnd) {
             ++jumps;
-            curEnd = farthest;
+            layerEnd = nextLayerEnd;
         }
     }
     return jumps;
