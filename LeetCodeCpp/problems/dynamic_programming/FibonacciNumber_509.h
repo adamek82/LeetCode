fb1a8a844313ea8fcbb158f46d10f75c03e7d42e
adamek82 <amichalski@gmail.com>
2026-09-19 11:56:30 +0200

    Remove verbose Fibonacci Number comments

--- LeetCodeCpp/problems/dynamic_programming/FibonacciNumber_509.h
+++ LeetCodeCpp/problems/dynamic_programming/FibonacciNumber_509.h
@@ -1,9 +1,5 @@
 #pragma once
 
-/*
- * Fibonacci Number (LeetCode 509)
- * Iterative DP, O(n) time, O(1) space. Safe in 32-bit int for n ≤ 30.
- */
 class FibonacciNumber_509 {
 public:
     int fib(int n);
